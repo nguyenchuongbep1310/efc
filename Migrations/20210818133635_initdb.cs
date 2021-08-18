4a9ff70f2d6f53f505e14ffea5ad09d193c2afa8
nguyenchuongbep1310 <67274198+nguyenchuongbep1310@users.noreply.github.com>
2021-08-18 21:20:37 +0700

    ByC

---
+++ Migrations/20210818133635_initdb.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Bogus;
+using chuong.models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efc.Migrations
+{
+    public partial class initdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Content = table.Column<string>(type: "ntext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_articles", x => x.Id);
+                });
+                //Insert Data
+                //Fake Data: Bogus
+
+                Randomizer.Seed = new Random(8675309);
+
+                var fakerArticle = new Faker<Article>();
+                fakerArticle.RuleFor(a => a.Title, f =>f.Lorem.Sentence(5, 5));
+                fakerArticle.RuleFor(a => a.Created, f => f.Date.Between(new DateTime(2021,1,1), new DateTime(2021,7,30)));
+                fakerArticle.RuleFor(a => a.Content, f => f.Lorem.Paragraphs(1, 4));
+
+                
+                
+                for(int i=0;i<=150;i++)
+                {
+                    Article article = fakerArticle.Generate();
+                    migrationBuilder.InsertData(
+                    table: "articles",
+                    columns: new [] {"Title", "Created", "Content"},
+                    values: new object[]{
+                        article.Title,
+                        article.Created,
+                        article.Content
+                    }
+                );
+                }
+
+                // migrationBuilder.InsertData(
+                //     table: "articles",
+                //     columns: new [] {"Title", "Created", "Content"},
+                //     values: new object[]{
+                //         "Bai viet 1",
+                //         new DateTime(2021,8,20),
+                //         "Noi dung 1"
+                //     }
+                // );
+
+
+                
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "articles");
+        }
+    }
+}
---
+++ Models/MyBlogContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace chuong.models
+{
+    public class MyBlogContext: DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options): base(options)
+        {
+            //
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Article> articles{get;set;}
+        
+    }
+}
