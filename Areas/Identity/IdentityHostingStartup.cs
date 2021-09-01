d6099a951073ffbb311db0d96bc0e7f233e5c3aa
nguyenchuongbep1310 <67274198+nguyenchuongbep1310@users.noreply.github.com>
2021-09-01 20:56:11 +0700

    Add Identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using chuong.models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(efc.Areas.Identity.IdentityHostingStartup))]
+namespace efc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Models/MyBlogContext.cs
+++ Models/MyBlogContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace chuong.models
 {
-    public class MyBlogContext: DbContext
+    public class MyBlogContext: IdentityDbContext<AppUser>
     {
         public MyBlogContext(DbContextOptions<MyBlogContext> options): base(options)
         {
@@ -17,6 +18,15 @@ namespace chuong.models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+            {   
+                var tablename= entityType.GetTableName();
+                if(tablename.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tablename.Substring(6));
+                }
+            }
         }
         public DbSet<Article> articles{get;set;}
         
